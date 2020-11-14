6239adead67bb1f6c7738dfc892eb898a65f6533
Aref <arefrf2010@gmail.com>
2020-11-14 17:49:09 +0330

    types

--- src/crm/index.ts
+++ src/crm/index.ts
@@ -1,65 +1,23 @@
 import qs from "querystringify";
 import { Base } from "../base";
 import {
-  instance,
-  NewInstance,
-  UpdateInstance,
-  Product,
-  NewProduct,
-  CreatedProduct,
-  DeletedProduct,
-  UpdateProduct,
-  UpdatedProduct,
-  Transaction,
-  NewTransaction,
-  CreatedTransaction,
-  UpdateTransaction,
-  UpdatedTransaction,
-  Invoice,
-  Invoices,
-  NewInvoice,
-  CreatedInvoice,
-  DeletedInvoice,
-  UpdateInvoice,
-  UpdatedInvoice,
-  Customer,
-  NewCustomer,
-  CreatedCustomer,
-  UpdateCustomer,
-  UpdatedCustomer,
-  sub_user,
-  sub_users,
-  NewUser,
-  CreatedUser,
-  DeletedUser,
-  UpdateUser,
-  addresses,
-  NewAddress,
-  DeletedAddress,
-  UpdateAddress,
-  SearchParams,
-  RevenueParams,
-  TotalParams,
-  SeriesParams,
-  ShopPageParams,
+  SendSmsParams,
+  SendTypedSmsParams,
+  sms,
 } from "./types";
 
 const resourceName = "crm";
 
 export class Instance extends Base {
   // ---------- SHOP ----------
-  sendSms(shop_id: number) {
+  sendSms(shop_id: number, params: SendSmsParams) {
     const path = `${resourceName}/${shop_id}/send/normal`;
-    return this.post<{sent: boolean, err: string, price: number}>(path);
+    return this.post<sms>(path, params);
   }
 
-  sendTypedSms(type, params: NewInstance) {
-    const path = `${resourceName}/${shop_id}/send/normal`;
-    return this.post<instance>(path, params);
+  sendTypedSms(shop_id: number, type, params: SendTypedSmsParams) {
+    const path = `${resourceName}/${shop_id}/send/typed/${type}`;
+    return this.post<sms>(path, params);
   }
 
-  updateInstance(params: UpdateInstance) {
-    let path = `${resourceName}/shop/update`;
-    return this.post<instance>(path, params);
-  }
 }
