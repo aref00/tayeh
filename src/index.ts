98d8a2474f74e27241482ee11ba69506fef1a1c2
Aref <arefrf2010@gmail.com>
2020-11-16 20:13:05 +0330

    crm

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { Media } from './media'
 import { Instance } from './instance'
 import { User } from './user' 
 import { Customer } from './customer'
+import { Crm } from './crm'
 import { applyMixins } from './utils'
 import { Base } from './base'
 
 class Tayeh extends Base {}
-interface Tayeh extends Instance, User, Media, Customer {}
-applyMixins(Tayeh, [Instance, User, Media, Customer]);
+interface Tayeh extends Instance, User, Media, Customer, Crm {}
+applyMixins(Tayeh, [Instance, User, Media, Customer, Crm]);
 
 export default Tayeh;
