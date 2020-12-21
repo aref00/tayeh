eefea2767612f5a5718bb648e9c8dae6fbe51c4f
Saeed <saeed.salehi1996@gmail.com>
2020-12-21 17:28:27 +0330

    customer new methods

--- src/customer/index.d.ts
+++ src/customer/index.d.ts
@@ -1,5 +1,12 @@
 import { Base } from "../base";
 import { customer } from "./types";
 export declare class Customer extends Base {
-    getCustomerMe(instance_id: number): Promise<customer>;
+    getPassword(username: string): Promise<import("axios").AxiosResponse<{
+        access_token: any;
+    }>>;
+    customerVerify(usename: string, password: string): Promise<import("axios").AxiosResponse<{
+        access_token: any;
+    }>>;
+    getCustomerMe(): Promise<import("axios").AxiosResponse<customer>>;
+    getCostomerAddresses(customer_id: number): Promise<import("axios").AxiosResponse<any[]>>;
 }
--- src/customer/index.ts
+++ src/customer/index.ts
@@ -1,11 +1,32 @@
 import qs from "querystringify";
 import { Base } from "../base";
-import { customer } from "./types"
+import { customer } from "./types";
 
 const resourceName = "customer";
 
 export class Customer extends Base {
-    getCustomerMe(instance_id: number) {
-        return this.get<customer>(`${resourceName}/${instance_id}/me`);
-      }
+  getPassword(username: string) {
+    const url = "customer/login";
+    return this.post_auth<{ access_token }>(url, {
+      username: username,
+    });
+  }
+
+  customerVerify(usename: string, password: string) {
+    const url = "customer/verify";
+    return this.post_auth<{ access_token }>(url, {
+      username: usename,
+      password: password,
+    });
+  }
+
+  getCustomerMe() {
+    return this.get<customer>(`${resourceName}/me`);
+  }
+
+  getCostomerAddresses(customer_id: number) {
+    let query = `${resourceName}/addresses/me`;
+    console.log(query);
+    return this.get<any[]>(query);
+  }
 }
