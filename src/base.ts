b7c928aafe64cc570c812684d67f24bf88763379
Saeed <saeed.salehi1996@gmail.com>
2020-12-21 17:30:11 +0330

    algorithm is complete

--- src/base.ts
+++ src/base.ts
@@ -1,11 +1,4 @@
-// import fetch from "isomorphic-unfetch";
-import Axios, { AxiosRequestConfig } from "axios";
-
-type Config = {
-  API_KEY: string;
-  API_SECRET: string;
-  API_ACCESS: string;
-};
+import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export type Pagination = {
   page?: number;
@@ -20,32 +13,34 @@ export abstract class Base {
   private mediaPath: string;
   private API_ACCESS: string;
 
-  constructor(config: Config) {
-    this.API_KEY = config.API_KEY;
-    this.API_SECRET = config.API_SECRET;
-    this.API_ACCESS =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEiLCJzdWIiOiJhM2NmZWEzMS05NzE2LTRhOTAtYjA2MS03NGVhZWNmOWQzNzIiLCJyb2xlIjoidXNlciIsIlRPS0VOX1RZUEUiOiJhcGkiLCJpYXQiOjE2MDgyMDE1NDIsImV4cCI6MTYwODI4Nzk0Mn0.rxKjAY2WSjbjNo5RUPjjVr4mavCkzMwoP_td5LRow90";
-    this.basePath = process.env.BASE_URL;
-    this.authPath = process.env.AUTH_URL;
-    this.mediaPath = process.env.MEDIA_URL;
+  constructor() {
+    this.API_KEY = process.env.TAYEH_API_KEY;
+    this.API_SECRET = process.env.TAYEH_API_SECRET;
+    this.basePath = process.env.TAYEH_BASE_URL;
+    this.authPath = process.env.TAYEH_AUTH_URL;
+    this.mediaPath = process.env.TAYEH_MEDIA_URL;
     if (typeof this.API_KEY !== "string" || typeof this.API_SECRET !== "string")
-      throw new Error("You should define API_KEY & API_SECRET.");
-    // this.get_token();
+      throw new Error(
+        "You should define TAYEH_API_KEY & TAYEH_API_SECRET in your environment variables."
+      );
+    this.get_token();
   }
 
   async get_token(): Promise<boolean> {
     const url = "api/token";
     try {
-      const { access_token } = await this.get_auth<{ access_token }>(
-        url,
-        {
-          auth: {
-            username: this.API_KEY,
-            password: this.API_SECRET,
+      const { access_token } = (
+        await this.get_auth<{ access_token }>(
+          url,
+          {
+            auth: {
+              username: this.API_KEY,
+              password: this.API_SECRET,
+            },
           },
-        },
-        true
-      );
+          true
+        )
+      ).data;
       this.API_ACCESS = access_token;
       return true;
     } catch (err) {
@@ -53,10 +48,14 @@ export abstract class Base {
     }
   }
 
+  set_token(access_token) {
+    this.API_ACCESS = access_token;
+  }
+
   protected async delete<T>(
     endpoint: string,
     options?: AxiosRequestConfig
-  ): Promise<T> {
+  ): Promise<AxiosResponse<T>> {
     const url = this.authPath + endpoint;
     const headers = {
       Authorization: `Bearer ${this.API_ACCESS}`,
@@ -68,23 +67,20 @@ export abstract class Base {
       ...options,
     };
     const res = await Axios.delete(url, config);
-    return res.data;
+    return res;
   }
 
   protected async get<T>(
     endpoint: string,
     options?: AxiosRequestConfig,
     isBasic: boolean = false
-  ): Promise<T> {
+  ): Promise<AxiosResponse<T>> {
     const url = this.basePath + endpoint;
-    console.log(url);
 
     const headers: any = {
       "Content-type": "application/json",
     };
     if (!isBasic) {
-      console.log(this.API_ACCESS);
-
       headers.Authorization = `Bearer ${this.API_ACCESS}`;
     }
     const config: AxiosRequestConfig = {
@@ -92,17 +88,15 @@ export abstract class Base {
       ...options,
     };
     const res = await Axios.get(url, config);
-    return res.data;
+    return res;
   }
 
   protected async get_auth<T>(
     endpoint: string,
     options?: AxiosRequestConfig,
     isBasic: boolean = false
-  ): Promise<T> {
+  ): Promise<AxiosResponse<T>> {
     const url = this.authPath + endpoint;
-    console.log(url);
-
     const headers: any = {
       "Content-type": "application/json",
     };
@@ -114,17 +108,15 @@ export abstract class Base {
       ...options,
     };
     const res = await Axios.get(url, config);
-    console.log(res.data);
-
-    return res.data;
+    return res;
   }
 
   protected async post<T>(
     endpoint: string,
     body = {},
     options?: AxiosRequestConfig
-  ): Promise<T> {
-    const url = this.authPath + endpoint;
+  ): Promise<AxiosResponse<T>> {
+    const url = this.basePath + endpoint;
     const headers = {
       Authorization: `Bearer ${this.API_ACCESS}`,
       "Content-type": "application/json",
@@ -135,27 +127,24 @@ export abstract class Base {
       ...options,
     };
     const res = await Axios.post(url, body, config);
-    return res.data;
+    return res;
   }
 
-  //   protected request<T>(endpoint: string, options?: AxiosRequestConfig): Promise<T> {
-  //     const url = this.basePath + endpoint;
-  //     const headers = {
-  //       // @todo change based on our api
-  //       Authorization: this.apiKey,
-  //       "Content-type": "application/json",
-  //     };
-
-  //     const config = {
-  //       ...options,
-  //       headers,
-  //     };
-
-  //     return fetch(url, config).then((r) => {
-  //       if (r.ok) {
-  //         return r.json();
-  //       }
-  //       throw new Error(r.statusText);
-  //     });
-  //   }
+  protected async post_auth<T>(
+    endpoint: string,
+    body = {},
+    options?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    const url = this.authPath + endpoint;
+    const headers = {
+      Authorization: `Bearer ${this.API_ACCESS}`,
+      "Content-type": "application/json",
+    };
+    const config: AxiosRequestConfig = {
+      headers: { ...headers },
+      ...options,
+    };
+    const res = await Axios.post(url, body, config);
+    return res;
+  }
 }
